26832aa4561c3f29565b31a8e0055e709df37369
A412 <max.mf.feng@berkeley.edu>
2015-03-01 14:34:46 -0800

    Problem

---
+++ app/src/main/java/com/example/chase/mathbash/Problem.java
@@ -0,0 +1,42 @@
+package com.example.chase.mathbash;
+
+public class Problem {
+  private String question;
+  private int answer;
+  public Problem(String q, int a) {
+    question = q;
+    answer = a;
+  }
+
+  public Problem(String q, String a) {
+    question = q;
+    try {
+      answer = Integer.parseInt(a);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  public boolean checkAnswer(Number ans) {
+    return (answer == ans.intValue());
+  }
+
+  public boolean checkAnswer(int ans) {
+    return (answer == ans);
+  }
+
+  public static Problem generate() {
+    String operation;
+    int ans;
+    int first = (int)(500 * Math.random());
+    int second = (int)(500 * Math.random());
+    if (Math.random() > 0.5) {
+      operation = " + ";
+      ans = first + second;
+    } else {
+      operation = " * ";
+      ans = first * second;
+    }
+    return new Problem(first + operation + second, ans);
+  }
+}
