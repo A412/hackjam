e0d9566d8108cc3a223c1dc615106b2015e25b52
Chase <chasesmith@berkeley.edu>
2015-03-06 21:54:07 -0800

    Created Drawproblem

--- app/src/main/java/com/example/chase/mathbash/LocationTimedProblemList.java
+++ app/src/main/java/com/example/chase/mathbash/LocationTimedProblemList.java
@@ -4,6 +4,11 @@ package com.example.chase.mathbash;
  * Created by Chase on 3/6/15.
  */
 public class LocationTimedProblemList extends TimedProblemList {
+
+    public LocationTimedProblemList(int max) {
+        super(max);
+    }
+
     @Override
     public void addProblem() {
         super.addProblem(LocationTimedProblem.generate());
